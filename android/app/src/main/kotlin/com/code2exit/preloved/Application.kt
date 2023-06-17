45c29aaa29f786aadc9dd3b12c6dca459268fd96
prayuk <pxpsoft@gmail.com>
2023-06-17 20:53:18 +0700

    v
    v

--- android/app/src/main/kotlin/com/code2exit/preloved/Application.kt
+++
@@ -1,6 +0,0 @@
-package com.code2exit.preloved
-import io.flutter.app.FlutterApplication
-class App : FlutterApplication() {
-
-
-}
--- android/app/src/main/kotlin/com/code2exit/preloved/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.code2exit.preloved
-import io.flutter.embedding.android.FlutterActivity
-class MainActivity: FlutterActivity() {
-
-}
